ab64b019b896a4a784426672a4bd5017dac9301f
Miles Tallia (RIT Student) <mot7506@rit.edu>
2024-09-08 23:46:23 -0400

    I think I did this out of order

--- src/test/java/coffeemaker/RecipeTest.java
+++ src/test/java/coffeemaker/RecipeTest.java
@@ -34,15 +34,16 @@ public class RecipeTest {
 
 
 
-	
+
 
 	// Basic
 
 	@Test
-	@DisplayName("Test 1 - Recipe Exists")
+	@DisplayName("Test 1 - Recipe exists")
 	public void test1() {
 		assertTrue(recipe != null);
 	}
+
 
 
 
@@ -134,6 +135,258 @@ public class RecipeTest {
 	public void test13() {
 		recipe.setPrice("7");
 		assertEquals(recipe.getPrice(), 7);
+	}
+
+
+
+
+
+
+	// Negative input for integer field
+
+	@Test
+	@DisplayName("Test 14 - Negative input for chocolate")
+	public void test14() {
+		try {
+			recipe.setAmtChocolate("-7");
+		} catch (RecipeException e) {
+		}
+		assertEquals(recipe.getAmtChocolate(), 0);
+	}
+
+	@Test
+	@DisplayName("Test 15 - Negative input for coffee")
+	public void test15() {
+		try {
+			recipe.setAmtCoffee("-7");
+		} catch (RecipeException e) {
+		}
+		assertEquals(recipe.getAmtChocolate(), 0);
+	}
+
+	@Test
+	@DisplayName("Test 16 - Negative input for milk")
+	public void test16() {
+		try {
+			recipe.setAmtMilk("-7");
+		} catch (RecipeException e) {
+		}
+		assertEquals(recipe.getAmtMilk(), 0);
+	}
+
+	@Test
+	@DisplayName("Test 17 - Negative input for sugar")
+	public void test17() {
+		try {
+			recipe.setAmtSugar("-7");
+		} catch (RecipeException e) {
+		}
+		assertEquals(recipe.getAmtSugar(), 0);
+	}
+
+	@Test
+	@DisplayName("Test 18 - Negative input for price")
+	public void test18() {
+		try {
+			recipe.setPrice("-7");
+		} catch (RecipeException e) {
+		}
+		assertEquals(recipe.getPrice(), 0);
+	}
+
+
+
+
+
+
+	// Non-numbered input for integer field
+
+	@Test
+	@DisplayName("Test 19 - Negative input for chocolate")
+	public void test19() {
+		try {
+			recipe.setAmtChocolate("seven");
+		} catch (RecipeException e) {
+		}
+		assertEquals(recipe.getAmtChocolate(), 0);
+	}
+
+	@Test
+	@DisplayName("Test 20 - Negative input for coffee")
+	public void test20() {
+		try {
+			recipe.setAmtCoffee("seven");
+		} catch (RecipeException e) {
+		}
+		assertEquals(recipe.getAmtChocolate(), 0);
+	}
+
+	@Test
+	@DisplayName("Test 21 - Negative input for milk")
+	public void test21() {
+		try {
+			recipe.setAmtMilk("seven");
+		} catch (RecipeException e) {
+		}
+		assertEquals(recipe.getAmtMilk(), 0);
+	}
+
+	@Test
+	@DisplayName("Test 22 - Negative input for sugar")
+	public void test22() {
+		try {
+			recipe.setAmtSugar("seven");
+		} catch (RecipeException e) {
+		}
+		assertEquals(recipe.getAmtSugar(), 0);
+	}
+
+	@Test
+	@DisplayName("Test 23 - Negative input for price")
+	public void tes23() {
+		try {
+			recipe.setPrice("seven");
+		} catch (RecipeException e) {
+		}
+		assertEquals(recipe.getPrice(), 0);
+	}
+
+
+
+
+
+
+	// ToString
+	
+	@Test
+	@DisplayName("Test 24 - Recipe prints")
+	public void test24() {
+		recipe.setName("NewName");
+		assertEquals(recipe.toString(), "Recipe{NewName}");
+	}
+	
+	@Test
+	@DisplayName("Test 25 - Blank recipe prints")
+	public void test25() {
+		assertEquals(recipe.toString(), "Recipe{}");
+	}
+
+
+
+
+
+
+	// Hashcode 
+
+	@Test
+	@DisplayName("Test 26 - Same hashcode equal")
+	public void test26() {
+		Recipe recipe2 = new Recipe();
+		recipe.setName("1");
+		recipe2.setName("1");
+		assertEquals(recipe.hashCode(), recipe2.hashCode());
+	}
+
+	@Test
+	@DisplayName("Test 27 - Different hashcodes do not equal")
+	public void test27() {
+		Recipe recipe2 = new Recipe();
+		recipe.setName("1");
+		recipe2.setName("2");
+		assertNotEquals(recipe.hashCode(), recipe2.hashCode());
+	}
+
+	// Equals
+
+	@Test
+	@DisplayName("Test 28 - Same recipes equal")
+	public void test28() {
+		Recipe recipe2 = new Recipe();
+		recipe.setName("1");
+		recipe2.setName("1");
+		assertEquals(recipe, recipe2);
+	}
+
+	@Test
+	@DisplayName("Test 29 - Same recipes equal function")
+	public void test29() {
+		Recipe recipe2 = new Recipe();
+		recipe.setName("1");
+		recipe2.setName("1");
+		assertEquals(true, recipe.equals(recipe2));
+	}
+
+	@Test
+	@DisplayName("Test 30 - Different recipes do not equal")
+	public void test30() {
+		Recipe recipe2 = new Recipe();
+		recipe.setName("1");
+		recipe2.setName("2");
+		assertNotEquals(recipe, recipe2);
+	}
+
+	@Test
+	@DisplayName("Test 31 - Different recipes do not equal function")
+	public void test31() {
+		Recipe recipe2 = new Recipe();
+		recipe.setName("1");
+		recipe2.setName("2");
+		assertEquals(false, recipe.equals(recipe2));
+	}
+
+	@Test
+	@DisplayName("Test 32 - Null recipe does not equal function")
+	public void test32() {
+		assertEquals(false, recipe.equals(null));
+	}
+
+	@Test
+	@DisplayName("Test 33 - Non recipe class does not equal function")
+	public void test33() {
+		RecipeBook rb = new RecipeBook();
+		assertEquals(false, recipe.equals(rb));
+	}
+
+	@Test
+	@DisplayName("Test 34 - Same null recipes equal")
+	public void test34() {
+		Recipe recipe2 = new Recipe();
+		assertEquals(recipe, recipe2);
+	}
+
+	@Test
+	@DisplayName("Test 35 - Same name but different ingredient recipes equal")
+	public void test35() {
+		Recipe recipe2 = new Recipe();
+		recipe.setName("flan");
+		recipe2.setName("flan");
+		recipe.setAmtMilk("20");
+		recipe2.setAmtMilk("10");
+		assertEquals(recipe, recipe2);
+	}
+
+	@Test
+	@DisplayName("Test 36 - Same name but different price recipes equal")
+	public void test36() {
+		Recipe recipe2 = new Recipe();
+		recipe.setName("tart");
+		recipe2.setName("tart");
+		recipe.setPrice("20");
+		recipe2.setPrice("10");
+		assertEquals(recipe, recipe2);
+	}
+
+	@Test
+	@DisplayName("Test 37 - Different names but same stats recipes equal")
+	public void test37() {
+		Recipe recipe2 = new Recipe();
+		recipe.setName("Milk");
+		recipe2.setName("Leche");
+		recipe.setPrice("10");
+		recipe2.setPrice("10");
+		recipe.setAmtMilk("100");
+		recipe2.setAmtMilk("100");
+		assertNotEquals(recipe, recipe2);
 	}
 
 }
